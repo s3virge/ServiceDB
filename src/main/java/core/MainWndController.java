587d5b74fd973bbe53feb6ed709aa203f82b369c
s3virge <kobzarvit@gmail.com>
2017-10-16 22:56:51 +0300

    рисовал главное окно...

--- src/main/java/core/MainWndController.java
+++ src/main/java/core/MainWndController.java
@@ -1,12 +1,28 @@
 package core;
 
-import core.LoginWndController;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import org.apache.log4j.Logger;
 
 public class MainWndController {
     private static final Logger logger = Logger.getLogger(LoginWndController.class);
 
+    @FXML private ListView deviceNumbersList;
+    @FXML private Button btnFindDeviceNum;
+    @FXML private TextField devNumToFind;
+
     public MainWndController() {
         logger.debug("execute core.MainWndController constructor");
+    }
+
+    @FXML
+    private void handleFindDeviceNumber(){
+        Alert msg = new Alert(Alert.AlertType.INFORMATION);
+        msg.setHeaderText(null);
+        msg.setContentText("Нажали на кнопку найти номер ремонта.");
+        msg.showAndWait();
     }
 }
